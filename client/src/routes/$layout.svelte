a7b65ddee4a741e367e95bef1a1ba46e66624fd7
Нуфтанчики <300225@mail.ru>
2021-05-04 01:07:38 +0300

    создал стили

---
+++ client/src/routes/$layout.svelte
@@ -0,0 +1,21 @@
+<script>
+    import Header from './../lib/base/header/Header.svelte';
+    import Footer from './../lib/base/footer/Footer.svelte';
+    import '../static/style/scss.svelte';
+</script>
+
+
+<div class="wrapper">
+    <div class="header">
+        <Header/>
+    </div>
+
+    <div class="content">
+        <slot/>
+    </div>
+
+    <div class="footer">
+        <Footer/>
+    </div>
+</div>
+
---
+++ client/src/static/style/scss.svelte
@@ -0,0 +1,11 @@
+<style global length="scss">
+
+@import "reset.css";
+@import "main.css";
+
+.content{
+    color: red;
+}
+
+
+</style>
