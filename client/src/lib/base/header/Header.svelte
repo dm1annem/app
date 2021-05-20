daebc627ac9195d26a664ee07f15dedec45fa4ba
Нуфтанчики <300225@mail.ru>
2021-05-20 22:56:23 +0300

    продолжаю копаться с хедером

--- client/src/lib/base/header/Header.svelte
+++ client/src/lib/base/header/Header.svelte
@@ -1,46 +1,49 @@
-<header class="flex flex-wrap flex-row justify-between md:items-center md:space-x-4 bg-white py-6 px-6 relative">
+<header class="grid grid-cols-6 grid-rows-2"> <!--flex flex-wrap flex-row justify-between md:items-center md:space-x-4 bg-white  px-8 relative shadow-xl-->
   <a href="#" class="block">
-    <span class="sr-only">themes.dev</span>
     <img class="h-6 md:h-8" src="https://www.karkascentr.ru/images/logo3.jpg" alt="Логотип КаркасЦентр" title="Логотип КаркасЦентр">
   </a>
   <button class="inline-block md:hidden w-8 h-8 bg-gray-200 text-gray-600 p-1">
     <svg fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
   </button>
 
-  <div >
-    <span >Проектирование и строительство загородных домов</span>
- </div>
- <p> Мы в социальных сетях: &nbsp &nbsp
-  <a href="https://vk.com/karkascentr"><i class="fa fa-vk" aria-hidden="true" ></i></a>&nbsp
-  <a href="https://ok.ru/karkascentr.ru"><i class="fa fa-odnoklassniki" aria-hidden="true"></i></a>&nbsp
-  <a href="https://www.youtube.com/channel/UCJoHMcmHq115v7XriMA5ZiQ?view_as=subscriber"><i class="fa fa-youtube-square" aria-hidden="true"></i></a>&nbsp
-  <a href="https://www.instagram.com/karkascentr.ru/"><i class="fa fa-instagram" aria-hidden="true"></i></a>&nbsp
-  <a href="https://www.facebook.com/karkascentr/"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-</p>	
-
- <div>
-
- </div>
-
- <div class="contacts-head"> 
-  <p>
-      <a href="https://wa.me/message/S2HVLZUQSNBBN1">
-          WhatsApp &nbsp <i class="fa fa-whatsapp" aria-hidden="true"></i> 
-      </a>
-  </p> 
-  <p>
-          <a href="tel:+79308200225">
-              +7(4832)30-02-25 &nbsp <i class="fa fa-phone"></i> 
-          </a>	
-  </p>
-  <p>
-          <a type=mail href="mailto:info@karkascentr.ru"> 
-               info@karkascentr.ru &nbsp <i class="fa fa-envelope" aria-hidden="true"></i> 
+  <div>
+    <p> <!--Мы в социальных сетях: &nbsp &nbsp -->
+      <a href="https://vk.com/karkascentr"><i class="fa fa-vk" aria-hidden="true" ></i></a>&nbsp
+      <a href="https://ok.ru/karkascentr.ru"><i class="fa fa-odnoklassniki" aria-hidden="true"></i></a>&nbsp
+      <a href="https://www.youtube.com/channel/UCJoHMcmHq115v7XriMA5ZiQ?view_as=subscriber"><i class="fa fa-youtube-square" aria-hidden="true"></i></a>&nbsp
+      <a href="https://www.instagram.com/karkascentr.ru/"><i class="fa fa-instagram" aria-hidden="true"></i></a>&nbsp
+      <a href="https://www.facebook.com/karkascentr/"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+    </p>	
+    
+    
+     <div class=""> 
+      <p>
+        <a href="#">
+            Заказать звонок <i class="fa fa-whatsapp" aria-hidden="true"></i> 
+        </a>
+      </p>
+      <p>
+          <a href="https://wa.me/message/S2HVLZUQSNBBN1">
+              Написать в WhatsApp &nbsp <i class="fa fa-whatsapp" aria-hidden="true"></i> 
           </a>
-  </p>
-      
-  				
-</div>
+      </p> 
+      <p>
+              <a href="tel:+79308200225">
+                  +7(4832)30-02-25 &nbsp <i class="fa fa-phone"></i> 
+              </a>	
+      </p>		
+      </div>
+  </div>
+
+ <nav class="absolute left-0 top-16 w-full bg-white"> <!--absolute md:relative top-16 left-0 md:top-0 z-20 flex flex-col md:flex-row md:space-x-6 font-semibold w-full md:w-auto bg-white shadow-md rounded-lg md:rounded-none md:shadow-none md:bg-transparent p-6 pt-0 md:p-0 -->
+  <a href="#" class="block py-1 text-indigo-600 hover:underline">Главная</a>
+  <a href="#" class="block py-1 text-gray-600 hover:underline">Проекты</a>
+  <a href="#" class="block py-1 text-gray-600 hover:underline">Примеры работы</a>
+  <a href="#" class="block py-1 text-gray-600 hover:underline">Цены</a>
+  <a href="#" class="block py-1 text-gray-600 hover:underline">Контакты</a>
+</nav>
+
+ 
   
 
   <div>
@@ -48,10 +51,3 @@
   </div>
 </header>
 
-<nav class="absolute md:relative top-16 left-0 md:top-0 z-20 flex flex-col md:flex-row md:space-x-6 font-semibold w-full md:w-auto bg-white shadow-md rounded-lg md:rounded-none md:shadow-none md:bg-transparent p-6 pt-0 md:p-0 ">
-  <a href="#" class="block py-1 text-indigo-600 hover:underline">Главная</a>
-  <a href="#" class="block py-1 text-gray-600 hover:underline">Проекты</a>
-  <a href="#" class="block py-1 text-gray-600 hover:underline">Примеры работы</a>
-  <a href="#" class="block py-1 text-gray-600 hover:underline">Цены</a>
-  <a href="#" class="block py-1 text-gray-600 hover:underline">Контакты</a>
-</nav>
